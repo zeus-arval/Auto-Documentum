593a40c09f63cad420269c96b9890c39cc60fda0
Artur Valdna <artur.valdna@playtech.com>
2022-07-19 10:22:53 +0300

    Added Console project

---
+++ src/ConsoleForTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AD.Soft.FilesManager;
+using Microsoft.Extensions.Logging;
+
+Console.WriteLine("Hello, World!");
+LoggerFactory factory = new LoggerFactory();
+
+
+FileReader reader = new FileReader(LoggerFactoryExtensions.CreateLogger<FileReader>(factory);
+
+string filePath = "";
+reader.ReadFile(filePath);
